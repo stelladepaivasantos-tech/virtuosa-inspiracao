8ad08af17f427d9317e3da819a454a1a10df3ec5
stelladepaivasantos-tech <stelladepaivasantos@gmail.com>
2026-08-07 16:24:21 -0300

    Create main.jsx

---
+++ `src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VirtuosaStore from '../virtuosa-loja.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VirtuosaStore />
+  </React.StrictMode>,
+)
